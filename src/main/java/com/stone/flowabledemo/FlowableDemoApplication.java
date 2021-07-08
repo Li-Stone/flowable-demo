63947ef4631e007457dedba734181de9839fe034
lbstone <libin.2017@foxmail.com>
2021-07-09 07:41:19 +0800

    初始化

---
+++ src/main/java/com/stone/flowabledemo/FlowableDemoApplication.java
@@ -0,0 +1,16 @@
+package com.stone.flowabledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class FlowableDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableDemoApplication.class, args);
+    }
+
+}
