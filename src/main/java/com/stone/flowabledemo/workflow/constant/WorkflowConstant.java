44d8467ab46f2e2641d1becee1b47a1d6349da16
lbstone <libin.2017@foxmail.com>
2021-07-11 01:20:19 +0800

    工作流常量

---
+++ src/main/java/com/stone/flowabledemo/workflow/constant/WorkflowConstant.java
@@ -0,0 +1,20 @@
+package com.stone.flowabledemo.workflow.constant;
+
+
+/**
+ * 工作流常量
+ *
+ * @author Administrator
+ */
+public class WorkflowConstant {
+
+    public static final String SINGLE_INSTANCE_NEW = "新建";
+    public static final String SINGLE_INSTANCE_EVAL = "评估中";
+    public static final String SINGLE_INSTANCE_EVAL_APPROVED = "评估通过";
+    public static final String SINGLE_INSTANCE_EVAL_REJECTED = "评估不通过";
+
+
+    public static final String SINGLE_ID_EVAL = "_eval";
+    public static final String SINGLE_ID_APPROVED = "_approved";
+    public static final String SINGLE_ID_REJECTED = "_rejected";
+}
