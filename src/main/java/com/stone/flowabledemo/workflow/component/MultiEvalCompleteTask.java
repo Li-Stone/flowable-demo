9aaffcd0932ffeecb6123589d50bdb76969edbae
libin <libin.2017@foxmail.com>
2021-07-12 15:09:44 +0800

    多实例流程任务完成条件判断逻辑完善

--- src/main/java/com/stone/flowabledemo/workflow/component/MultiEvalCompleteTask.java
+++ src/main/java/com/stone/flowabledemo/workflow/component/MultiEvalCompleteTask.java
@@ -1,5 +1,7 @@
 package com.stone.flowabledemo.workflow.component;
 
+import com.stone.flowabledemo.workflow.constant.WorkflowConstant;
+import org.flowable.common.engine.api.FlowableException;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.springframework.stereotype.Component;
 
@@ -14,10 +16,25 @@ public class MultiEvalCompleteTask implements Serializable {
     private static final long serialVersionUID = -858029427755527737L;
 
 
-    public boolean accessCondition(DelegateExecution execution){
+    public boolean accessCondition(DelegateExecution execution) {
         int sum = (int) execution.getVariable("nrOfInstances");
         int activeNum = (int) execution.getVariable("nrOfActiveInstances");
         int complete = (int) execution.getVariable("nrOfCompletedInstances");
+        String currentActivityId = execution.getCurrentActivityId();
+        if (!currentActivityId.equals(WorkflowConstant.MULTI_ID_EVAL)) {
+            throw new FlowableException(execution.getProcessInstanceId() + "任务定义id异常");
+        }
+        if (sum != complete) {
+            return false;
+        }
+        if (execution.getVariable(WorkflowConstant.VAR_KEY_EVAL_RESULT) != null) {
+            if ((int) execution.getVariable(WorkflowConstant.VAR_KEY_EVAL_REJECTED) > 0) {
+                // 有拒绝
+                execution.setVariable(WorkflowConstant.VAR_KEY_OUTCOME, WorkflowConstant.EVAL_RESULT_REJECTED);
+                return true;
+            }
+        }
+        execution.setVariable(WorkflowConstant.VAR_KEY_OUTCOME, WorkflowConstant.EVAL_RESULT_APPROVED);
         return true;
     }
 }
--- src/main/java/com/stone/flowabledemo/workflow/constant/WorkflowConstant.java
+++ src/main/java/com/stone/flowabledemo/workflow/constant/WorkflowConstant.java
@@ -21,9 +21,15 @@ public class WorkflowConstant {
     public static final String SINGLE_ID_APPROVED = "_approved";
     public static final String SINGLE_ID_REJECTED = "_rejected";
 
+    public static final String MULTI_ID_NEW = "_new";
+    public static final String MULTI_ID_EVAL = "_eval";
+    public static final String MULTI_ID_APPROVED = "_approved";
+    public static final String MULTI_ID_REJECTED = "_rejected";
+
     public static final String VAR_KEY_PRINCIPAL = "principal";
     public static final String VAR_KEY_OUTCOME = "outcome";
 
     public static final String VAR_KEY_EVAL_REJECTED = "rejected";
     public static final String VAR_KEY_EVAL_CURRENT_RESULT = "currentResult";
+    public static final String VAR_KEY_EVAL_RESULT = "evalResult";
 }
